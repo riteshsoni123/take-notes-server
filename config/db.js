c88202e0d770b20ed039e3f1a2b818b6fc654108
riteshsoni123 <comedybuddy1@gmail.com>
2023-05-05 17:35:37 +0530

    added strictQuery false

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
+
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGO_URI);
   console.log("MongoDB connected");
